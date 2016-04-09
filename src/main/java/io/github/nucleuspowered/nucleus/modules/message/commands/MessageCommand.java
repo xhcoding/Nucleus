a8e7a126ac670635f16cdb87562ece8482e454a0
Daniel Naylor <git@drnaylor.co.uk>
2016-04-09 11:40:09 +0100

    Complete descriptions, fix missed command.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/message/commands/MessageCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/message/commands/MessageCommand.java
@@ -9,14 +9,14 @@ import io.github.nucleuspowered.nucleus.argumentparsers.PlayerConsoleArgument;
 import io.github.nucleuspowered.nucleus.internal.annotations.Permissions;
 import io.github.nucleuspowered.nucleus.internal.annotations.RegisterCommand;
 import io.github.nucleuspowered.nucleus.internal.annotations.RunAsync;
-import io.github.nucleuspowered.nucleus.internal.command.OldCommandBase;
+import io.github.nucleuspowered.nucleus.internal.command.CommandBase;
 import io.github.nucleuspowered.nucleus.internal.permissions.SuggestedLevel;
 import io.github.nucleuspowered.nucleus.modules.message.handlers.MessageHandler;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.CommandElement;
 import org.spongepowered.api.command.args.GenericArguments;
-import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.text.Text;
 
 /**
@@ -27,19 +27,18 @@ import org.spongepowered.api.text.Text;
 @Permissions(suggestedLevel = SuggestedLevel.USER)
 @RunAsync
 @RegisterCommand({ "message", "m", "msg", "whisper", "w", "tell", "t" })
-public class MessageCommand extends OldCommandBase<CommandSource> {
+public class MessageCommand extends CommandBase<CommandSource> {
     private final String to = "to";
     private final String message = "message";
 
     @Inject private MessageHandler handler;
 
     @Override
-    public CommandSpec createSpec() {
-        return getSpecBuilderBase()
-            .arguments(
+    public CommandElement[] getArguments() {
+        return new CommandElement[] {
                 new PlayerConsoleArgument(Text.of(to)),
                 GenericArguments.onlyOne(GenericArguments.remainingJoinedStrings(Text.of(message)))
-            ).description(Text.of("Send a message to a player")).build();
+        };
     }
 
     @Override
