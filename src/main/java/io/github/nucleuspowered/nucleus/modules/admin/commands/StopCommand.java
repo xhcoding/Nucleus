c8370ecb394f38d5df113705f92c521772173262
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2016-04-10 13:09:46 -0400

    Add /stop

---
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/admin/commands/StopCommand.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
+package io.github.nucleuspowered.nucleus.modules.admin.commands;
+
+import java.util.Optional;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.CommandElement;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.serializer.TextSerializers;
+
+import io.github.nucleuspowered.nucleus.internal.annotations.Permissions;
+import io.github.nucleuspowered.nucleus.internal.annotations.RegisterCommand;
+import io.github.nucleuspowered.nucleus.internal.command.CommandBase;
+
+@Permissions
+@RegisterCommand({"stop"})
+public class StopCommand extends CommandBase<CommandSource> {
+    
+    private final String messageKey = "message";
+    
+    @Override
+    public CommandElement[] getArguments() {
+        return new CommandElement[] {
+                GenericArguments.optional(
+                        GenericArguments.remainingJoinedStrings(Text.of(messageKey)))
+        };
+    }
+    
+    @Override
+    public CommandResult executeCommand(CommandSource src, CommandContext args) throws Exception {
+        Optional<String> opt = args.<String>getOne(messageKey);
+        if (opt.isPresent()) {
+            Sponge.getServer().shutdown(TextSerializers.FORMATTING_CODE.deserialize(opt.get())); 
+        } else {
+            Sponge.getServer().shutdown();
+        }
+        return CommandResult.success();
+    }
+}
