20cb4da84120f60c9118036200ca3725d2e0e7b8
Daniel Naylor <git@drnaylor.co.uk>
2016-04-30 17:01:29 +0100

    Revert "Add better way to localise config strings."
    Turns out it doesn't work when saving again. This reverts commit e3cdf0f7beeadf5b2f14a6f7f1942818d0d6c53f.

--- src/main/java/io/github/nucleuspowered/nucleus/config/configurate/NucleusObjectMapper.java
+++ src/main/java/io/github/nucleuspowered/nucleus/config/configurate/NucleusObjectMapper.java
@@ -5,7 +5,6 @@
 package io.github.nucleuspowered.nucleus.config.configurate;
 
 import io.github.nucleuspowered.nucleus.Util;
-import io.github.nucleuspowered.nucleus.internal.annotations.LocalisedCommentSetting;
 import ninja.leaping.configurate.objectmapping.ObjectMapper;
 import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 import ninja.leaping.configurate.objectmapping.Setting;
@@ -14,7 +13,6 @@ import java.lang.reflect.Field;
 import java.util.Map;
 
 public class NucleusObjectMapper<T> extends ObjectMapper<T> {
-
     /**
      * Create a new object mapper of a given type
      *
@@ -27,64 +25,24 @@ public class NucleusObjectMapper<T> extends ObjectMapper<T> {
 
     protected void collectFields(Map<String, FieldData> cachedFields, Class<? super T> clazz) throws ObjectMappingException {
         for (Field field : clazz.getDeclaredFields()) {
-            Info setting = null;
             if (field.isAnnotationPresent(Setting.class)) {
-                setting = setting(field);
-            } else if (field.isAnnotationPresent(LocalisedCommentSetting.class)) {
-                setting = locSetting(field);
+                Setting setting = field.getAnnotation(Setting.class);
+                String path = setting.value();
+                if (path.isEmpty()) {
+                    path = field.getName();
+                }
+
+                String comment = setting.comment();
+                if (comment.startsWith("loc:")) {
+                    comment = Util.getMessageWithFormat(setting.comment().split(":", 2)[1]);
+                }
+
+                FieldData data = new FieldData(field, comment);
+                field.setAccessible(true);
+                if (!cachedFields.containsKey(path)) {
+                    cachedFields.put(path, data);
+                }
             }
-
-            if (setting != null) {
-                addField(cachedFields, field, setting.path, setting.comment);
-            }
-        }
-    }
-
-    private Info setting(Field field) {
-        Setting setting = field.getAnnotation(Setting.class);
-        String path = setting.value();
-        if (path.isEmpty()) {
-            path = field.getName();
-        }
-
-        String comment = setting.comment();
-        if (comment.startsWith("loc:")) {
-            comment = Util.getMessageWithFormat(setting.comment().split(":", 2)[1]);
-        }
-
-        return new Info(path, comment);
-    }
-
-    private Info locSetting(Field field) {
-        LocalisedCommentSetting setting = field.getAnnotation(LocalisedCommentSetting.class);
-        String path = setting.value();
-        if (path.isEmpty()) {
-            path = field.getName();
-        }
-
-        String comment = setting.messageKey();
-        if (!comment.isEmpty()) {
-            return new Info(path, Util.getMessageWithFormat(comment));
-        }
-
-        return new Info(path, "");
-    }
-
-    private void addField(Map<String, FieldData> cachedFields, Field field, String path, String comment) throws ObjectMappingException {
-        FieldData data = new FieldData(field, comment);
-        field.setAccessible(true);
-        if (!cachedFields.containsKey(path)) {
-            cachedFields.put(path, data);
-        }
-    }
-
-    private static class Info {
-        private final String path;
-        private final String comment;
-
-        public Info(String path, String comment) {
-            this.path = path;
-            this.comment = comment;
         }
     }
 }
