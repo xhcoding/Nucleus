77415d145fe6e3e0df9975ea3321b3a40e75ad8b
Daniel Naylor <git@drnaylor.co.uk>
2016-05-02 17:56:13 +0100

    Only show kits in /kit list that the user should have access to.
    Closes #125

--- src/main/java/io/github/nucleuspowered/nucleus/modules/kit/commands/KitListCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/kit/commands/KitListCommand.java
@@ -7,15 +7,16 @@ package io.github.nucleuspowered.nucleus.modules.kit.commands;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import io.github.nucleuspowered.nucleus.Util;
+import io.github.nucleuspowered.nucleus.internal.PermissionRegistry;
 import io.github.nucleuspowered.nucleus.internal.annotations.*;
 import io.github.nucleuspowered.nucleus.internal.command.CommandBase;
 import io.github.nucleuspowered.nucleus.internal.permissions.SuggestedLevel;
+import io.github.nucleuspowered.nucleus.modules.kit.config.KitConfigAdapter;
 import io.github.nucleuspowered.nucleus.modules.kit.handlers.KitHandler;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
-import org.spongepowered.api.command.args.CommandElement;
 import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.service.pagination.PaginationService;
 import org.spongepowered.api.text.Text;
@@ -24,6 +25,7 @@ import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.format.TextStyles;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Lists all the kits.
@@ -39,28 +41,28 @@ import java.util.ArrayList;
 public class KitListCommand extends CommandBase<CommandSource> {
 
     @Inject private KitHandler kitConfig;
-
-    @Override
-    public CommandElement[] getArguments() {
-        return super.getArguments();
-    }
+    @Inject private KitConfigAdapter kca;
 
     @Override
     public CommandResult executeCommand(final CommandSource src, CommandContext args) throws Exception {
         PaginationService paginationService = Sponge.getServiceManager().provide(PaginationService.class).get();
         ArrayList<Text> kitText = Lists.newArrayList();
 
-        for (String kit : kitConfig.getKitNames()) {
+        Set<String> kits = kitConfig.getKitNames();
+        if (kits.isEmpty()) {
+            src.sendMessage(Util.getTextMessageWithFormat("command.kit.list.empty"));
+            return CommandResult.empty();
+        }
+
+        // Only show kits that the user has permission for, if needed. This is the permission "nucleus.kits.<kit>".
+        kitConfig.getKitNames().stream()
+                .filter(kit -> !kca.getNodeOrDefault().isSeparatePermissions() || src.hasPermission(PermissionRegistry.PERMISSIONS_PREFIX + "kits." + kit.toLowerCase()))
+                .forEach(kit -> {
             Text item = Text.builder(kit).onClick(TextActions.runCommand("/kit " + kit))
                     .onHover(TextActions.showText(Util.getTextMessageWithFormat("command.kit.list.text", kit))).color(TextColors.AQUA)
                     .style(TextStyles.UNDERLINE).build();
             kitText.add(item);
-        }
-
-        if (kitText.isEmpty()) {
-            src.sendMessage(Util.getTextMessageWithFormat("command.kit.list.empty"));
-            return CommandResult.empty();
-        }
+        });
 
         PaginationList.Builder paginationBuilder = paginationService.builder().contents(kitText)
                 .title(Util.getTextMessageWithFormat("command.kit.list.kits")).padding(Text.of(TextColors.GREEN, "-"));
