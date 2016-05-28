139715d1aaf042350e010cc06015095cc02a3b83
Daniel Naylor <git@drnaylor.co.uk>
2016-05-28 18:28:40 +0100

    Move fly into its own module.
    * Add fly on world change check.
    * Allow disabling fly completely to allow players to use EssentialCmds at the same time (still)

--- src/main/java/io/github/nucleuspowered/nucleus/modules/fly/commands/FlyCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/fly/commands/FlyCommand.java
@@ -2,7 +2,7 @@
  * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
  * at the root of this project for more details.
  */
-package io.github.nucleuspowered.nucleus.modules.misc.commands;
+package io.github.nucleuspowered.nucleus.modules.fly.commands;
 
 import io.github.nucleuspowered.nucleus.Util;
 import io.github.nucleuspowered.nucleus.internal.annotations.Permissions;
