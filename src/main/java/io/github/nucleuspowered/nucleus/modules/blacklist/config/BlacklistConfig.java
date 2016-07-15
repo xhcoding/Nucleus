48da06f546bfbf44d3a89728811bf50c0edba554
Hassan Syyid <hassansyyid@gmail.com>
2016-07-15 16:02:15 -0400

    Fixed placing blacklisted blocks and added option to specify an item to replace blacklisted items with (rather than just removing the item). Closes #202

--- src/main/java/io/github/nucleuspowered/nucleus/modules/blacklist/config/BlacklistConfig.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/blacklist/config/BlacklistConfig.java
@@ -6,6 +6,8 @@ package io.github.nucleuspowered.nucleus.modules.blacklist.config;
 
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
 
 @ConfigSerializable
 public class BlacklistConfig {
@@ -15,6 +17,12 @@ public class BlacklistConfig {
 
     @Setting(comment = "loc:config.blacklist.inventory")
     private boolean inventory = true;
+    
+    @Setting(comment = "loc:config.blacklist.use-replacement")
+    private boolean useReplacement = false;
+
+    @Setting(comment = "loc:config.blacklist.replacement")
+    private ItemType replacement = ItemTypes.DIRT;
 
     public boolean isEnvironment() {
         return environment;
@@ -22,5 +30,13 @@ public class BlacklistConfig {
 
     public boolean isInventory() {
         return inventory;
+    }
+
+    public boolean shouldUseReplacement() {
+        return useReplacement;
+    }
+
+    public ItemType getReplacement() {
+        return replacement;
     }
 }
