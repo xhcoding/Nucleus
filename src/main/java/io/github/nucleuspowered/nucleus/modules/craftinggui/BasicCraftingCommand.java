ca4540203ceb5a49ee2aded6404dd5a8c72a3342
Daniel Naylor <git@drnaylor.co.uk>
2016-10-20 23:06:31 +0100

    Add crafting GUIs.
    Fixes #434

---
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/craftinggui/BasicCraftingCommand.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
+package io.github.nucleuspowered.nucleus.modules.craftinggui;
+
+import io.github.nucleuspowered.nucleus.internal.command.AbstractCommand;
+import io.github.nucleuspowered.nucleus.internal.command.ReturnMessageException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.event.cause.NamedCause;
+import org.spongepowered.api.item.inventory.Inventory;
+import org.spongepowered.api.item.inventory.InventoryArchetype;
+
+public abstract class BasicCraftingCommand extends AbstractCommand<Player> {
+
+    protected abstract InventoryArchetype getArchetype();
+
+    @Override public CommandResult executeCommand(Player src, CommandContext args) throws Exception {
+        Inventory i = Inventory.builder().of(getArchetype())
+            .build(plugin);
+        src.openInventory(i, Cause.of(NamedCause.owner(plugin))).orElseThrow(() -> new ReturnMessageException(plugin.getMessageProvider().getTextMessageWithFormat("command.crafting.error")));
+        return CommandResult.success();
+    }
+}
---
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/craftinggui/commands/WorkbenchCommand.java
@@ -0,0 +1,20 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
+package io.github.nucleuspowered.nucleus.modules.craftinggui.commands;
+
+import io.github.nucleuspowered.nucleus.internal.annotations.Permissions;
+import io.github.nucleuspowered.nucleus.internal.annotations.RegisterCommand;
+import io.github.nucleuspowered.nucleus.modules.craftinggui.BasicCraftingCommand;
+import org.spongepowered.api.item.inventory.InventoryArchetype;
+import org.spongepowered.api.item.inventory.InventoryArchetypes;
+
+@Permissions
+@RegisterCommand("workbench")
+public class WorkbenchCommand extends BasicCraftingCommand {
+
+    @Override protected InventoryArchetype getArchetype() {
+        return InventoryArchetypes.WORKBENCH;
+    }
+}
