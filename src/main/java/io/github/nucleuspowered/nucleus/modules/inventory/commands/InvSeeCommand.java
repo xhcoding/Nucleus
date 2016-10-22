cdfd886e78fc3d20a1657c53ff8923048f5b769d
Daniel Naylor <git@drnaylor.co.uk>
2016-10-22 16:40:11 +0100

    Add ability to view offline inventories using /invsee.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/inventory/commands/InvSeeCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/inventory/commands/InvSeeCommand.java
@@ -22,6 +22,7 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.args.CommandElement;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.NamedCause;
 import org.spongepowered.api.text.Text;
@@ -45,19 +46,24 @@ public class InvSeeCommand extends AbstractCommand<Player> {
         Map<String, PermissionInformation> mspi = super.permissionSuffixesToRegister();
         mspi.put("exempt.target", new PermissionInformation(plugin.getMessageProvider().getMessageWithFormat("permission.invsee.exempt.inspect"), SuggestedLevel.ADMIN));
         mspi.put("exempt.interact", new PermissionInformation(plugin.getMessageProvider().getMessageWithFormat("permission.invsee.exempt.interact"), SuggestedLevel.ADMIN));
+        mspi.put("offline", new PermissionInformation(plugin.getMessageProvider().getMessageWithFormat("permission.invsee.offline"), SuggestedLevel.ADMIN));
         return mspi;
     }
 
     @Override
     public CommandElement[] getArguments() {
         return new CommandElement[] {
-            GenericArguments.onlyOne(new NicknameArgument(Text.of(player), udm, NicknameArgument.UnderlyingType.PLAYER))
+            GenericArguments.onlyOne(new NicknameArgument(Text.of(player), udm, NicknameArgument.UnderlyingType.USER))
         };
     }
 
     @Override
     public CommandResult executeCommand(Player src, CommandContext args) throws Exception {
-        Player target = args.<Player>getOne(player).get();
+        User target = args.<User>getOne(player).get();
+
+        if (!target.isOnline() && permissions.testSuffix(src, "offline")) {
+            throw new ReturnMessageException(plugin.getMessageProvider().getTextMessageWithFormat("command.invsee.nooffline"));
+        }
 
         if (target.getUniqueId().equals(src.getUniqueId())) {
             throw new ReturnMessageException(plugin.getMessageProvider().getTextMessageWithFormat("command.invsee.self"));
@@ -67,7 +73,8 @@ public class InvSeeCommand extends AbstractCommand<Player> {
             throw new ReturnMessageException(plugin.getMessageProvider().getTextMessageWithFormat("command.invsee.targetexempt", target.getName()));
         }
 
-        src.openInventory(target.getInventory(), Cause.of(NamedCause.of("plugin", plugin), NamedCause.source(src)));
+        // Just in case, get the player inventory if they are online.
+        src.openInventory(target.isOnline() ? target.getPlayer().get().getInventory() : target.getInventory(), Cause.of(NamedCause.of("plugin", plugin), NamedCause.source(src)));
         return CommandResult.success();
     }
 }
