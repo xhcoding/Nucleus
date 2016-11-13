872d00d7b1dea21fde9b3aa86b0c8e86a629c123
Daniel Naylor <git@drnaylor.co.uk>
2016-11-13 15:50:40 +0000

    Force /skull to use /minecraft:give on 1.8.9
    This commit will be removed/reverted on 1.10.2.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/item/config/SkullConfig.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/item/config/SkullConfig.java
@@ -4,14 +4,16 @@
  */
 package io.github.nucleuspowered.nucleus.modules.item.config;
 
+import io.github.nucleuspowered.nucleus.configurate.annotations.DoNotGenerate;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class SkullConfig {
 
+    @DoNotGenerate
     @Setting(value = "use-minecraft-command", comment = "loc:config.item.skullcompat")
-    private boolean useMinecraftCommand = false;
+    private boolean useMinecraftCommand = true;
 
     public boolean isUseMinecraftCommand() {
         return useMinecraftCommand;
