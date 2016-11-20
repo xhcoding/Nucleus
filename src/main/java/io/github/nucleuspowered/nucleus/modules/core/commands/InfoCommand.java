c9f76fc9f593f04ab46789c5935dfd81e08489d4
Daniel Naylor <git@drnaylor.co.uk>
2016-11-20 15:02:10 +0000

    Add /nucleus info for dumping some environment + nucleus info.

---
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/core/commands/InfoCommand.java
@@ -0,0 +1,99 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
+package io.github.nucleuspowered.nucleus.modules.core.commands;
+
+import com.google.common.collect.Lists;
+import io.github.nucleuspowered.nucleus.PluginInfo;
+import io.github.nucleuspowered.nucleus.internal.annotations.NoCooldown;
+import io.github.nucleuspowered.nucleus.internal.annotations.NoCost;
+import io.github.nucleuspowered.nucleus.internal.annotations.NoWarmup;
+import io.github.nucleuspowered.nucleus.internal.annotations.Permissions;
+import io.github.nucleuspowered.nucleus.internal.annotations.RegisterCommand;
+import io.github.nucleuspowered.nucleus.internal.annotations.RunAsync;
+import io.github.nucleuspowered.nucleus.internal.command.AbstractCommand;
+import io.github.nucleuspowered.nucleus.internal.permissions.SuggestedLevel;
+import org.spongepowered.api.Platform;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.util.TextMessageException;
+import uk.co.drnaylor.quickstart.ModuleContainer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Set;
+
+@Permissions(prefix = "nucleus", suggestedLevel = SuggestedLevel.NONE)
+@RunAsync
+@NoWarmup
+@NoCooldown
+@NoCost
+@RegisterCommand(value = "info", subcommandOf = NucleusCommand.class)
+public class InfoCommand extends AbstractCommand<CommandSource> {
+
+    private final String sep = "------------";
+
+    @Override public CommandResult executeCommand(CommandSource src, CommandContext args) throws Exception {
+        // Sponge versions
+        List<String> information = Lists.newArrayList();
+
+        information.add(sep);
+        information.add("Nucleus Diagnostics");
+        information.add(sep);
+
+        information.add("This file contains information about Nucleus and the environment it runs in.");
+
+        information.add(sep);
+        information.add("Environment");
+        information.add(sep);
+
+        Platform platform = Sponge.getPlatform();
+        information.add("Minecraft Version: " + platform.getMinecraftVersion().getName());
+        information.add(String.format("Sponge Version: %s %s", platform.getImplementation().getName(), platform.getImplementation().getVersion().orElse("unknown")));
+        information.add(String.format("Sponge API Version: %s %s", platform.getApi().getName(), platform.getApi().getVersion().orElse("unknown")));
+        information.add("Nucleus Version: " + PluginInfo.VERSION + " (Git: " + PluginInfo.GIT_HASH + ")");
+
+        information.add(sep);
+        information.add("Plugins");
+        information.add(sep);
+
+        Sponge.getPluginManager().getPlugins().forEach(x -> information.add(x.getName() + " (" + x.getId() + ") version " + x.getVersion().orElse("unknown")));
+
+        information.add(sep);
+        information.add("Nucleus: Enabled Modules");
+        information.add(sep);
+
+        plugin.getModuleContainer().getModules().stream().sorted().forEach(information::add);
+
+        Set<String> disabled = plugin.getModuleContainer().getModules(ModuleContainer.ModuleStatusTristate.DISABLE);
+        if (!disabled.isEmpty()) {
+            information.add(sep);
+            information.add("Nucleus: Disabled Modules");
+            information.add(sep);
+
+            disabled.stream().sorted().forEach(information::add);
+        }
+
+
+        String fileName = "nucleus-info-" + DateTimeFormatter.BASIC_ISO_DATE.format(LocalDateTime.now()) + "-" + DateTimeFormatter.ofPattern("HHmmss").format(LocalDateTime.now()) + ".txt";
+        try (BufferedWriter fw = new BufferedWriter(new FileWriter(fileName, false))) {
+            for (String s : information) {
+                fw.write(s);
+                fw.newLine();
+            }
+
+            fw.flush();
+        } catch (Exception e) {
+            throw new TextMessageException(plugin.getMessageProvider().getTextMessageWithFormat("command.nucleus.info.fileerror"), e);
+        }
+
+        src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.nucleus.info.saved", fileName));
+        return CommandResult.success();
+    }
+}
