0a414f6b453c82ced3e8ef8f281e237be972734c
Daniel Naylor <git@drnaylor.co.uk>
2016-11-20 18:56:11 +0000

    Add ability to disable fly on login for those without /fly permission.
    * Tries to find a safe location for those who are about to fall.
    
    Closes #477

--- src/main/java/io/github/nucleuspowered/nucleus/modules/fly/config/FlyConfig.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/fly/config/FlyConfig.java
@@ -13,7 +13,21 @@ public class FlyConfig {
     @Setting(value = "save-all-flystate-on-quit", comment = "loc:config.fly.stateonquit")
     private boolean saveOnQuit = true;
 
+    @Setting(value = "find-safe-location-on-login", comment = "loc:config.fly.onlogin")
+    private boolean findSafeOnLogin = true;
+
+    @Setting(value = "require-fly-permission-on-login", comment = "loc:config.fly.permissiononlogin")
+    private boolean permissionOnLogin = false;
+
     public boolean isSaveOnQuit() {
         return saveOnQuit;
+    }
+
+    public boolean isPermissionOnLogin() {
+        return permissionOnLogin;
+    }
+
+    public boolean isFindSafeOnLogin() {
+        return findSafeOnLogin;
     }
 }
