5710266d349c6e8faa05d39c1b559c3da231b95d
Daniel Naylor <git@drnaylor.co.uk>
2016-11-21 23:33:51 +0000

    Add more descriptive error on failure.
    * Properly disable everything on error.

--- src/main/java/io/github/nucleuspowered/nucleus/NucleusPlugin.java
+++ src/main/java/io/github/nucleuspowered/nucleus/NucleusPlugin.java
@@ -53,6 +53,7 @@ import io.github.nucleuspowered.nucleus.util.ThrowableAction;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
+import org.spongepowered.api.Platform;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.asset.Asset;
 import org.spongepowered.api.config.ConfigDir;
@@ -62,16 +63,20 @@ import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.service.permission.PermissionDescription;
 import org.spongepowered.api.service.permission.PermissionService;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 import uk.co.drnaylor.quickstart.enums.ConstructionPhase;
 import uk.co.drnaylor.quickstart.exceptions.IncorrectAdapterTypeException;
 import uk.co.drnaylor.quickstart.exceptions.NoModuleException;
 import uk.co.drnaylor.quickstart.exceptions.QuickStartModuleDiscoveryException;
-import uk.co.drnaylor.quickstart.exceptions.QuickStartModuleLoaderException;
 import uk.co.drnaylor.quickstart.modulecontainers.DiscoveryModuleContainer;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -85,7 +90,7 @@ public class NucleusPlugin extends Nucleus {
 
     private Instant gameStartedTime = null;
     private boolean modulesLoaded = false;
-    private boolean isErrored = false;
+    private Throwable isErrored = null;
     private CommandsConfig commandsConfig;
     private GeneralService generalService;
     private ItemDataService itemDataService;
@@ -149,7 +154,8 @@ public class NucleusPlugin extends Nucleus {
             chatUtil = new ChatUtil(this);
             nameUtil = new NameUtil(this);
         } catch (Exception e) {
-            isErrored = true;
+            isErrored = e;
+            disable();
             e.printStackTrace();
             return;
         }
@@ -187,14 +193,15 @@ public class NucleusPlugin extends Nucleus {
 
             moduleContainer.startDiscover();
         } catch (QuickStartModuleDiscoveryException e) {
-            isErrored = true;
+            isErrored = e;
+            disable();
             e.printStackTrace();
         }
     }
 
     @Listener
     public void onPostInit(GamePostInitializationEvent event) {
-        if (isErrored) {
+        if (isErrored != null) {
             return;
         }
 
@@ -208,7 +215,8 @@ public class NucleusPlugin extends Nucleus {
             // Reload so that we can update the serialisers.
             moduleContainer.reloadSystemConfig();
         } catch (Exception e) {
-            isErrored = true;
+            isErrored = e;
+            disable();
             e.printStackTrace();
             return;
         }
@@ -217,10 +225,15 @@ public class NucleusPlugin extends Nucleus {
             Sponge.getEventManager().post(new BaseModuleEvent.AboutToConstructEvent(this));
             logger.info(messageProvider.getMessageWithFormat("startup.moduleloading", PluginInfo.NAME));
             moduleContainer.loadModules(false);
-        } catch (QuickStartModuleLoaderException.Construction | QuickStartModuleLoaderException.Enabling construction) {
+
+            if (moduleContainer.getConfigAdapterForModule("core", CoreConfigAdapter.class).getNodeOrDefault().isErrorOnStartup()) {
+                throw new IllegalStateException("In main.conf, core.simulate-error-on-startup is set to TRUE. Remove this config entry to allow Nucleus to start. Simulating error and disabling Nucleus.");
+            }
+        } catch (Exception construction) {
             logger.info(messageProvider.getMessageWithFormat("startup.modulenotloaded", PluginInfo.NAME));
             construction.printStackTrace();
-            isErrored = true;
+            disable();
+            isErrored = construction;
             return;
         }
 
@@ -238,7 +251,7 @@ public class NucleusPlugin extends Nucleus {
 
     @Listener
     public void onGameStarted(GameStartedServerEvent event) {
-        if (!isErrored) {
+        if (isErrored == null) {
             try {
                 // Save any additions.
                 moduleContainer.refreshSystemConfig();
@@ -252,7 +265,7 @@ public class NucleusPlugin extends Nucleus {
 
     @Listener
     public void onServerStop(GameStoppedServerEvent event) {
-        if (!isErrored) {
+        if (isErrored == null) {
             this.gameStartedTime = null;
             logger.info(messageProvider.getMessageWithFormat("startup.stopped", PluginInfo.NAME));
             saveData();
@@ -504,5 +517,72 @@ public class NucleusPlugin extends Nucleus {
                 m.entrySet().stream().filter(x -> x.getValue().level == SuggestedLevel.USER).forEach(k -> ops.get().newDescriptionBuilder(this).get().assign(PermissionDescription.ROLE_USER, true).description(k.getValue().description).id(k.getKey()).register());
             }
         }
+    }
+
+    private void disable() {
+        // Disable everything, just in case. Thanks to pie-flavor: https://forums.spongepowered.org/t/disable-plugin-disable-itself/15831/8
+        Sponge.getEventManager().unregisterPluginListeners(this);
+        Sponge.getCommandManager().getOwnedBy(this).forEach(Sponge.getCommandManager()::removeMapping);
+        Sponge.getScheduler().getScheduledTasks(this).forEach(Task::cancel);
+
+        // Re-register this to warn people about the error.
+        Sponge.getEventManager().registerListener(this, GameStartedServerEvent.class, e -> errorOnStartup());
+    }
+
+    private void errorOnStartup() {
+        Sponge.getServer().setHasWhitelist(true);
+        Sponge.getServer().getConsole().sendMessages(getErrorMessage());
+    }
+
+    private List<Text> getErrorMessage() {
+        List<Text> messages = Lists.newArrayList();
+        messages.add(Text.of(TextColors.RED, "----------------------------"));
+        messages.add(Text.of(TextColors.RED, "-  NUCLEUS FAILED TO LOAD  -"));
+        messages.add(Text.of(TextColors.RED, "----------------------------"));
+        messages.add(Text.EMPTY);
+        messages.add(Text.of(TextColors.RED, "Nucleus encountered an error during server start up and did not enable successfully. No commands, listeners or tasks are registered."));
+        messages.add(Text.of(TextColors.RED, "The server has been automatically whitelisted - this is to protect your server and players if you rely on some of Nucleus' functionality (such as fly states, etc.)"));
+        messages.add(Text.of(TextColors.RED, "The error that Nucleus encountered will be reproduced below for your convenience."));
+
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        if (isErrored == null) {
+            messages.add(Text.of(TextColors.YELLOW, "No exception was saved."));
+        } else {
+            try (StringWriter sw = new StringWriter(); PrintWriter pw = new PrintWriter(sw)) {
+                isErrored.printStackTrace(pw);
+                pw.flush();
+                String[] stackTrace = sw.toString().split("(\r)?\n");
+                for (String s : stackTrace) {
+                    messages.add(Text.of(TextColors.YELLOW, s));
+                }
+            } catch (IOException e) {
+                isErrored.printStackTrace();
+            }
+        }
+
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        messages.add(Text.of(TextColors.RED, "If this error persists, check your configuration files and ensure that you have the latest version of Nucleus which matches the current version of the Sponge API."));
+        messages.add(Text.of(TextColors.RED, "If you do, please report this error to the Nucleus team at https://github.com/NucleusPowered/Nucleus/issues"));
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        messages.add(Text.of(TextColors.YELLOW, "Server Information"));
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        messages.add(Text.of(TextColors.YELLOW, "Nucleus version: " + PluginInfo.VERSION + ", (Git: " + PluginInfo.GIT_HASH + ")"));
+
+        Platform platform = Sponge.getPlatform();
+        messages.add(Text.of(TextColors.YELLOW, "Minecraft version: " + platform.getMinecraftVersion().getName()));
+        messages.add(Text.of(TextColors.YELLOW, String.format("Sponge Version: %s %s", platform.getImplementation().getName(), platform.getImplementation().getVersion().orElse("unknown"))));
+        messages.add(Text.of(TextColors.YELLOW, String.format("Sponge API Version: %s %s", platform.getApi().getName(), platform.getApi().getVersion().orElse("unknown"))));
+
+        messages.add(Text.EMPTY);
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        messages.add(Text.of(TextColors.YELLOW, "Installed Plugins"));
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        Sponge.getPluginManager().getPlugins().forEach(x -> messages.add(Text.of(TextColors.YELLOW, x.getName() + " (" + x.getId() + ") version " + x.getVersion().orElse("unknown"))));
+
+        messages.add(Text.EMPTY);
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        messages.add(Text.of(TextColors.YELLOW, "- END NUCLEUS ERROR REPORT -"));
+        messages.add(Text.of(TextColors.YELLOW, "----------------------------"));
+        return messages;
     }
 }
