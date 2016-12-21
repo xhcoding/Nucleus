34642fce406e92d6400d5b101a1b553e8a64ddf3
Daniel Naylor <git@drnaylor.co.uk>
2016-12-21 12:18:31 +0000

    Improve the documentation on the ConditionalListener annotation.

--- src/main/java/io/github/nucleuspowered/nucleus/internal/annotations/ConditionalListener.java
+++ src/main/java/io/github/nucleuspowered/nucleus/internal/annotations/ConditionalListener.java
@@ -6,11 +6,7 @@ package io.github.nucleuspowered.nucleus.internal.annotations;
 
 import io.github.nucleuspowered.nucleus.Nucleus;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.util.function.Predicate;
 
 /**
@@ -18,7 +14,8 @@ import java.util.function.Predicate;
  * us to try to keep the number of required listeners down to a minimum.
  *
  * <p>
- *     This may get loaded and/or unloaded during a reload event.
+ *     This will get unloaded and loaded during a reload event, but as this happens on the sync thread,
+ *     the listeners should get called regardless.
  * </p>
  */
 @Retention(RetentionPolicy.RUNTIME)
@@ -26,5 +23,17 @@ import java.util.function.Predicate;
 @Documented
 public @interface ConditionalListener {
 
+    /**
+     * The class that determines whether to load a listener or not. The {@link Predicate#test(Object)}
+     * method will determine whether the listener should be fired.
+     *
+     * <p>
+     *     This <strong>must</strong> be a No-Args constructor class, as the class will be instantiated
+     *     via reflection.
+     * </p>
+     *
+     * @return The {@link Class} of the {@link Predicate} that will determine whether a listener should
+     * be loaded.
+     */
     Class<? extends Predicate<Nucleus>> value();
 }
