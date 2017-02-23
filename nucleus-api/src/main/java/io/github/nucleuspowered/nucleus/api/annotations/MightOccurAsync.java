b60d1d3f0414f436941551205276d91108b81a1e
Daniel Naylor <git@drnaylor.co.uk>
2017-02-23 09:32:14 +0000

    How did I miss this...?

--- nucleus-api/src/main/java/io/github/nucleuspowered/nucleus/api/annotations/MightOccurAsync.java
+++ nucleus-api/src/main/java/io/github/nucleuspowered/nucleus/api/annotations/MightOccurAsync.java
@@ -1,3 +1,7 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
 package io.github.nucleuspowered.nucleus.api.annotations;
 
 import java.lang.annotation.Documented;
