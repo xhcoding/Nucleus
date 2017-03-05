2cc8c7a721affbe830397cb1caa7915ddefd681a
Hasaan Arshad <hasaan668@hotmail.co.uk>
2017-03-05 20:31:08 +0000

    Add optional player argument to /firstjoinkit redeem

--- src/main/java/io/github/nucleuspowered/nucleus/modules/kit/commands/firstjoin/FirstKitRedeemCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/kit/commands/firstjoin/FirstKitRedeemCommand.java
@@ -6,14 +6,11 @@ package io.github.nucleuspowered.nucleus.modules.kit.commands.firstjoin;
 
 import io.github.nucleuspowered.nucleus.Util;
 import io.github.nucleuspowered.nucleus.dataservices.KitService;
-import io.github.nucleuspowered.nucleus.internal.annotations.NoCooldown;
-import io.github.nucleuspowered.nucleus.internal.annotations.NoCost;
-import io.github.nucleuspowered.nucleus.internal.annotations.NoWarmup;
-import io.github.nucleuspowered.nucleus.internal.annotations.Permissions;
-import io.github.nucleuspowered.nucleus.internal.annotations.RegisterCommand;
+import io.github.nucleuspowered.nucleus.internal.annotations.*;
 import io.github.nucleuspowered.nucleus.internal.command.AbstractCommand;
 import io.github.nucleuspowered.nucleus.modules.kit.config.KitConfigAdapter;
 import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.util.Tristate;
@@ -26,21 +23,25 @@ import javax.inject.Inject;
 @NoCooldown
 @NoWarmup
 @RegisterCommand(value = "redeem", subcommandOf = FirstKitCommand.class)
-public class FirstKitRedeemCommand extends AbstractCommand<Player> {
-
-    @Inject
-    private KitService gds;
-
+public class FirstKitRedeemCommand extends AbstractCommand.SimpleTargetOtherPlayer {
+    @Inject private KitService gds;
     @Inject private KitConfigAdapter kca;
 
     @Override
-    public CommandResult executeCommand(Player src, CommandContext args) throws Exception {
+    protected CommandResult executeWithPlayer(CommandSource src, Player pl, CommandContext args, boolean isSelf) throws Exception {
+        Tristate tristate = Util.addToStandardInventory(pl, gds.getFirstKit(), false, kca.getNodeOrDefault().isProcessTokens());
 
-        Tristate tristate = Util.addToStandardInventory(src, gds.getFirstKit(), false, kca.getNodeOrDefault().isProcessTokens());
-
-        src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.firstkit.redeem.success"));
-        if (tristate != Tristate.TRUE) {
-            src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.firstkit.redeem.reject"));
+        if (isSelf) {
+            src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.firstkit.redeem.success"));
+            if (tristate != Tristate.TRUE) {
+                src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.firstkit.redeem.reject"));
+            }
+        } else {
+            src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.firstkit.give.spawned", pl.getName()));
+            pl.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.firstkit.give.success", src.getName()));
+            if (tristate != Tristate.TRUE) {
+                src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.firstkit.give.reject", pl.getName()));
+            }
         }
 
         return CommandResult.success();
