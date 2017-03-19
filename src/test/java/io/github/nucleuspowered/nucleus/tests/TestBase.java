18f78c190faedc29f0864270f6d6ade8a13fedd0
Daniel Naylor <git@drnaylor.co.uk>
2017-03-19 01:27:04 +0000

    Fix broken tests.

--- src/test/java/io/github/nucleuspowered/nucleus/tests/TestBase.java
+++ src/test/java/io/github/nucleuspowered/nucleus/tests/TestBase.java
@@ -221,6 +221,10 @@ public abstract class TestBase {
             return null;
         }
 
+        @Override public boolean traceUserCreations() {
+            return false;
+        }
+
         @Override public void registerReloadable(ThrowableAction<? extends Exception> reloadable) {
 
         }
