13a6570c0eb9b3ec30b302304f2139961c7efd89
Daniel Naylor <git@drnaylor.co.uk>
2017-05-16 21:39:21 +0100

    Remove the blacklist module, leaving only migrators.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/blacklist/BlacklistModule.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/blacklist/BlacklistModule.java
@@ -5,14 +5,13 @@
 package io.github.nucleuspowered.nucleus.modules.blacklist;
 
 import io.github.nucleuspowered.nucleus.api.service.NucleusBlacklistMigrationService;
-import io.github.nucleuspowered.nucleus.internal.qsml.module.ConfigurableModule;
-import io.github.nucleuspowered.nucleus.modules.blacklist.config.BlacklistConfigAdapter;
+import io.github.nucleuspowered.nucleus.internal.qsml.module.StandardModule;
 import io.github.nucleuspowered.nucleus.modules.blacklist.handler.BlacklistMigrationHandler;
 import org.spongepowered.api.Sponge;
 import uk.co.drnaylor.quickstart.annotations.ModuleData;
 
 @ModuleData(id = BlacklistModule.ID, name = "Blacklist")
-public class BlacklistModule extends ConfigurableModule<BlacklistConfigAdapter> {
+public class BlacklistModule extends StandardModule {
 
     public final static String ID = "blacklist";
 
@@ -20,7 +19,7 @@ public class BlacklistModule extends ConfigurableModule<BlacklistConfigAdapter> 
         plugin.getLogger().warn("-----------------------------------");
         plugin.getLogger().warn("NOTICE OF REMOVAL: BLACKLIST MODULE");
         plugin.getLogger().warn("-----------------------------------");
-        plugin.getLogger().warn("The item blacklist module will be removed from Nucleus soon. Please use some other protection plugin, such as "
+        plugin.getLogger().warn("The item blacklist module has been removed from Nucleus. Please use some other protection plugin, such as "
                 + "GriefPrevention that can block these items.");
         plugin.getLogger().warn("A migrator for those of you that use ProtectionPerms has been provided, run `/blacklist migrate pp` to "
                 + "attempt to migrate.");
@@ -30,10 +29,5 @@ public class BlacklistModule extends ConfigurableModule<BlacklistConfigAdapter> 
         BlacklistMigrationHandler bmh = new BlacklistMigrationHandler();
         plugin.getInternalServiceManager().registerService(BlacklistMigrationHandler.class, bmh);
         Sponge.getServiceManager().setProvider(plugin, NucleusBlacklistMigrationService.class, bmh);
-    }
-
-    @Override
-    public BlacklistConfigAdapter createAdapter() {
-        return new BlacklistConfigAdapter();
     }
 }
