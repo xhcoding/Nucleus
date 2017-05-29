0a63476437a7be06f67850f78e825b69bbcf524f
Daniel Naylor <git@drnaylor.co.uk>
2017-05-29 19:29:43 +0100

    No longer need to generate this config option.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/admin/config/AdminConfig.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/admin/config/AdminConfig.java
@@ -4,6 +4,7 @@
  */
 package io.github.nucleuspowered.nucleus.modules.admin.config;
 
+import io.github.nucleuspowered.neutrino.annotations.DoNotGenerate;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
@@ -13,6 +14,7 @@ public class AdminConfig {
     @Setting(value = "broadcast-message-template", comment = "config.broadcast.template")
     private BroadcastConfig broadcastMessage = new BroadcastConfig();
 
+    @DoNotGenerate
     @Setting(value = "separate-gamemode-permissions", comment = "config.gamemode.separate")
     private boolean separateGamemodePermission = true;
 
