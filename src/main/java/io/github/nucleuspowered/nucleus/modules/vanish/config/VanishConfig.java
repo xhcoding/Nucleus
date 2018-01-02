91285957d6d735405638803cf9721ac001ff8379
Daniel Naylor <git@drnaylor.co.uk>
2018-01-02 16:04:59 +0000

    Disable tab list altering on vanish.
    Update Neutrino to 1.1.0
    
    Fixes #1045, #1043, #1041, #1039, #1037

--- src/main/java/io/github/nucleuspowered/nucleus/modules/vanish/config/VanishConfig.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/vanish/config/VanishConfig.java
@@ -4,6 +4,7 @@
  */
 package io.github.nucleuspowered.nucleus.modules.vanish.config;
 
+import io.github.nucleuspowered.neutrino.annotations.RequiresProperty;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
@@ -13,8 +14,9 @@ public class VanishConfig {
     @Setting(value = "hide-connection-messages-on-vanish", comment = "config.vanish.connectionmessages")
     private boolean suppressMessagesOnVanish = false;
 
+    @RequiresProperty("nucleus.vanish.tablist.enable")
     @Setting(value = "alter-tab-list", comment = "config.vanish.altertablist")
-    private boolean alterTabList = true;
+    private boolean alterTabList = false;
 
     public boolean isSuppressMessagesOnVanish() {
         return suppressMessagesOnVanish;
